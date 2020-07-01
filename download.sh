8f50d55faba24b9a2909542d4b450e7675db63eb
Bram <bram@bram.codes>
2020-07-01 19:42:34 +0200

    Change youtube-dl

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
-/e/tools/youtube-dl.exe -U
-/e/tools/youtube-dl.exe "PLlr-m6aT0bkemifaU7ohKVJt3CAfr7nw8" --no-overwrites -f 'bestaudio[ext=m4a]/bestaudio' -o 'episodes/%(upload_date)s_%(id)s.%(ext)s' --write-info-json
+youtube-dl -U
+youtube-dl "PLlr-m6aT0bkemifaU7ohKVJt3CAfr7nw8" --no-overwrites -f 'bestaudio[ext=m4a]/bestaudio' -o 'episodes/%(upload_date)s_%(id)s.%(ext)s' --write-info-json
