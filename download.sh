9547828d7894fc690705d69a84947b13be7c3116
root <bram@brams.dev>
2020-07-27 21:00:46 +0200

    Add/update files (2020-07-27 21:00)

--- download.sh
+++ download.sh
@@ -1,2 +1,2 @@
 youtube-dl -U
-youtube-dl "PLlr-m6aT0bkemifaU7ohKVJt3CAfr7nw8" -f 'bestaudio[ext=m4a]/bestaudio' -o 'episodes/%(upload_date)s_%(id)s.%(ext)s' --write-info-json --download-archive download_archive.txt
+youtube-dl "PLlr-m6aT0bkemifaU7ohKVJt3CAfr7nw8" -f 'bestaudio[ext=m4a]/bestaudio' -o 'episodes/%(upload_date)s_%(id)s.%(ext)s' -i --write-info-json --download-archive download_archive.txt
